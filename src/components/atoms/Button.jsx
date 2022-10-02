9a771c7bab094bb4ccb801e576e50ee596ba14a7
Emilien <emilien.ginko@gmail.com>
2022-10-02 12:38:31 +0200

    🔧💄✨ responsive button, hover and increment

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -2,19 +2,29 @@ import React from "react";
 import Icon from "../atoms/Icon"
 import "../../assets/styles/components/atoms/buttons.scss"
 
-const Button = ({content, type, color})=>{
+const Button = ({content, type, color, value, callBack, maxValue}, ...props)=>{
+
+    const more = ()=>{
+        if( value < maxValue)
+            callBack(value + 1);
+    }
+    const less = ()=>{
+        if(value > 1)
+            callBack(value - 1);
+    }
+
     if(type === "svg") {
         return <button className={"button " + type + " " + color + " "}> <Icon type={content}/></button>
     }
     if(type === "text"){
-        return <button className={"button " + type + " " + color + " "}>{content}</button>
+        return <button className={"button " + type + " " + color + " "}><span className="span">{content}</span></button>
     }
 
     if(type === "qty-select"){
         return  <div className={"button " + type + " " + color + " "}>
-                    <button>-</button>
-                    0
-                    <button>+</button>
+                    <button className="less" onClick={less}><span className="span">-</span></button>
+                    {value}
+                    <button className="more" onClick={more}><span className="span">+</span></button>
                 </div>
 
     }
