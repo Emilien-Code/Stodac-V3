65041529139ebb71867ea35e1950aa08640efa92
Emilien <emilien.ginko@gmail.com>
2022-10-06 21:32:24 +0200

    💄 flex responsive boutique

--- src/assets/scripts/store/redux.js
+++ src/assets/scripts/store/redux.js
@@ -2,11 +2,13 @@ import {configureStore } from '@reduxjs/toolkit';
 import cartReducer from './redux-slices/cart';
 import modalsReducer from './redux-slices/modals';
 import authenticationReducer from './redux-slices/authentication';
+import articles from './redux-slices/articles';
 
 export default configureStore({
     reducer: {
         cart: cartReducer,
         modals: modalsReducer,
         authentication: authenticationReducer,
+        articles: articles,
     },
 })
