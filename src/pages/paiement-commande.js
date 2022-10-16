f2b42d4abbebacf6bac79218e3de051d7273e064
Emilien <emilien.ginko@gmail.com>
2022-10-16 12:11:25 +0200

    💄 cleaning Tobie's CSS

--- src/pages/paiement-commande.js
+++ src/pages/paiement-commande.js
@@ -2,13 +2,13 @@ import React from "react";
 import "../assets/styles/components/pages/payement-commande.scss"
 import Bloc from "../components/sections/bloc"
 const Payement = ()=>{
-    return <>
+    return <div className="payement-command">
         <div className="bloc1">
             <Bloc type="Facturation"/>
         </div>
         <div className="Bloc2">
             <Bloc type="Mode de payement"/>
         </div>
-    </>
+    </div>
 }
 export default Payement
