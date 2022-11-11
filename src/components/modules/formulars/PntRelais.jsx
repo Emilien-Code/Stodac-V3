1957bf12834e2fbbe8e6da89116e389131ec21c7
Emilien <emilien.ginko@gmail.com>
2022-11-11 14:46:33 +0100

    Point relais colissimo

---
+++ src/components/modules/formulars/PntRelais.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+const PntRelais = () => {
+    const widgetContainer = React.useRef(null)
+
+    React.useEffect(()=>{
+
+        callColissimo()
+
+    }, [])
+    
+    const callColissimo = ()=>{
+
+        const s = document.getElementsByTagName("script")
+        console.log(s)
+        fetch('https://stodac.fr/api-test/utils/token')
+        .then((response) => response.json())
+        .then((response) => {
+            window.$('#widget-container').frameColissimoOpen({
+            "ceLang" : "fr",
+            "URLColissimo": " https://ws.colissimo.fr",
+            "ceCountryList": "FR",
+            "callBackFrame": "noCallBack",
+            "ceCountry": "FR",
+            "dyPreparationTime": 1,
+            "token": response.token,
+          })
+        }).catch(()=>{
+            // callColissimo()
+        })
+      }
+
+
+    return (
+        <div id="ptnrelais">
+            <div id="widget-container" ref={widgetContainer}></div>
+            <input type="hidden" id="pudoWidgetErrorCode"/>
+            <input type="hidden" id="pudoWidgetAddress1"/>
+            <input type="hidden" id="pudoWidgetTown"/>
+            <input type="hidden" id="pudoWidgetZipCode"/>
+        </div>
+    )
+}
+
+export default PntRelais
