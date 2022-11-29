1c7e64ff74d43571994a28b89b8d7e283098982a
Emilien <emilien.ginko@gmail.com>
2022-11-29 12:16:43 +0100

    ✨ bubbles move with mouse

--- src/components/modules/cart.jsx
+++ src/components/modules/cart.jsx
@@ -5,13 +5,13 @@ import { useSelector, useDispatch } from "react-redux";
 import { setCart } from "../../assets/scripts/store/redux-slices/modals";
 import Article from "./article";
 import formatNumber from "../../assets/scripts/utils/priceNormalisation"
-
+import gsap from "gsap";
 const Cart = ()=>{
     const dispatch = useDispatch();
     const cartIsOpen = useSelector((state) => state.modals.cart);
     const cart = useSelector((state) => state.cart)
     const isEmpty = cart.cart.length <= 0 ? true : false
-    
+    const cartRef = React.useRef(null)
     const pushToCommande = ()=>{
         window.location.href = `/recapitulatif-commande`
     }
@@ -30,24 +30,152 @@ const Cart = ()=>{
             $body.style.removeProperty('top');
             $body.style.removeProperty('width');
         }
+
+        if(cartIsOpen){
+            const tl = gsap.timeline({})
+
+            if(!isEmpty){
+
+                document.querySelector(".cartNotEmpty").style.overflow = "hidden"
+                document.querySelector(".cartNotEmpty ul").style.overflow = "hidden"
+            
+            }
+
+
+            tl.to('.cart-wrapper', {
+                opacity: 1,
+                duration: .1
+            })
+            tl.fromTo(
+                cartRef.current,{
+                x: '120%',
+                y: -500,
+                height: '200%',
+                transformOrigin: "center",
+                rotate: '-5deg'
+            },{
+                x:0,
+                y:0,
+                height: '100%',
+                rotate: '0deg',
+                duration: 0.5
+            })
+            tl.fromTo('hr', {opacity: 0}, { opacity : 1, duration : .2})
+            tl.fromTo('.cart-header',{
+                y: 100
+            },{
+                y: 0,
+                duration: .4
+            })
+            if(isEmpty){
+
+            tl.fromTo(".h1-text", {
+                y: 140,
+                rotate: '12deg'
+            },{
+                y:0,
+                rotate: '0deg',
+                stagger: .1,
+                duration: .4
+            })
+            }
+            if(!isEmpty){
+
+                tl.fromTo(".cartNotEmpty li", {
+                    opacity: 0
+                },{
+                    opacity: 1,
+                    duration: .3
+                }).fromTo(".total",{ opacity : 0 },{ opacity : 1 })
+            
+            }
+
+            tl.fromTo(".cart-footer .button",{
+                y: 100
+            },{
+                y: 0,
+                duration: .4,
+                onConclude: ()=>  {
+                    if(!isEmpty)
+                        document.querySelector(".cartNotEmpty").style.overflow = "auto"
+                }
+            })
+
+        }
+
     }, [cartIsOpen])
 
     const close = ()=>{ 
-        dispatch(setCart(false))
+        const tl = gsap.timeline({})
+
+        tl.to(".cart-footer .button", {
+            y: -100,
+            duration: .4,
+
+        })
+
+        if(!isEmpty){
+
+            tl.to(".cartNotEmpty li", {
+                y: -20,
+                opacity: 0,
+                stagger: .3,
+            }).to(".total",{ opacity : 0 })
+  
+
+        }else{
+
+            tl.to(".h1-text", {
+                y: -110,
+                rotate: '12deg',
+                stagger: .1,
+                duration: .4
+            })
+            
+        }
+
+        tl.to('hr', {opacity: 0, duration : .2})
+        tl.to('.cart-header',{
+            y: 100,
+            duration: .4
+        })
+        tl.to(
+            cartRef.current,{
+            x: '120%',
+            y: -500,
+            height: '200%',
+            transformOrigin: "center",
+            rotate: '-5deg',
+            duration: 0.5
+        })
+        tl.to('.cart-wrapper', {
+            opacity: 0,
+            duration: .1,
+            onConclude: () => {
+                 dispatch(setCart(false))
+            }
+        })
+
     }
 
     if(cartIsOpen){
-        return <div className="cart-wrapper">
-            <aside className="cart">
-                <div className="cart-header">
-                    <h2>Panier</h2>
-                    <Button type="svg" content="cross" callBack={close}/>
+        return <div className="cart-wrapper" >
+            <aside className="cart" ref={cartRef}>
+                <div className="cart-header-mask">
+                    <div className="cart-header">
+                        <h2>Panier</h2>
+                        <Button type="svg" content="cross" callBack={close}/>
+                    </div>
                 </div>
                     <hr/>
-                <section className={`${isEmpty ? "isEmpty" : "" }`}>
+                <section className={`${isEmpty ? "isEmpty" : "cartNotEmpty" }`}>
                     {
                         isEmpty ? 
-                        <h1>Votre panier est vide</h1>: <ul>
+                        <h1>
+                            <div className="h1-mask"><span className="h1-text">Votre</span></div>
+                            <div className="h1-mask"><span className="h1-text">panier</span></div>
+                            <div className="h1-mask"><span className="h1-text">est vide</span></div>
+                        </h1>: <ul>
                             {
                                 cart.cart.map((article, index)=>{
                                     return <li key={index}><Article data={article} display="onCart"/></li>
@@ -64,7 +192,9 @@ const Cart = ()=>{
                             <span>Total</span> <span>{cart.total ? formatNumber(cart.total) + "€": " "}</span>
                         </div> : <></>
                     }
+                    <div className="btn-mask">
                     <Button type="text" callBack={isEmpty ? close : pushToCommande} content={isEmpty ? "Retourner à la boutique" : "Passer commande"} color="green"/>
+                    </div>
                 </div>
             </aside>
     </div>
