7aa061b6ccdc25dfad93c334c7156e8f1625ca6f
Emilien <emilien.ginko@gmail.com>
2022-11-30 15:58:33 +0100

    💄modals centered

--- src/components/modules/formulars/Modale.jsx
+++ src/components/modules/formulars/Modale.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import Button from "../../atoms/Button";
 import "../../../assets/styles/components/modules/formulars/modale.scss";
 const Modale = ({valid, unvalid, text, noCallBack, yesCallback})=> {
+    React.useEffect(()=>{
+        document.querySelector('.modale').style.top = `calc(${window.scrollY}px + 50vh)`
+    }, [])
+
     return <div className="modale">
         <h2>{text}</h2>
         <div>
