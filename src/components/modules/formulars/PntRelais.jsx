7a39ef8dd05bca67d319b126d17cb59cb3cab0db
Emilien <emilien.ginko@gmail.com>
2022-12-04 19:06:16 +0100

    pnt relais

--- src/components/modules/formulars/PntRelais.jsx
+++ src/components/modules/formulars/PntRelais.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+
 const PntRelais = () => {
-
     React.useEffect(()=>{
+        const script = document.createElement("script");
+        script.src = "https://ws.colissimo.fr/widget-point-retrait/resources/js/jquery.plugin.colissimo.min.js";
+        document.body.appendChild(script);
 
-        callColissimo()
+        script.addEventListener("load", ()=>{
+             console.log('script chargé')
+             callColissimo()
+        })
 
     }, [])
 
@@ -12,27 +18,34 @@ const PntRelais = () => {
         .then((response) => response.json())
         .then((response) => {
             window.$('#widget-container').frameColissimoOpen({
-            "ceLang" : "fr",
-            "URLColissimo": " https://ws.colissimo.fr",
-            "ceCountryList": "FR",
-            "callBackFrame": "noCallBack",
-            "ceCountry": "FR",
-            "dyPreparationTime": 1,
+                "ceLang" : "fr",
+                "URLColissimo": " https://ws.colissimo.fr",
+                "ceCountryList": "FR",
+                "callBackFrame": "noCallBack",
+                "ceCountry": "FR",
+                "dyPreparationTime": 1,
             "token": response.token,
           })
         }).catch(()=>{
-            // callColissimo()
         })
+
+      }
+      const test=(e)=>{
+            console.log(e.target.value)
       }
 
 
     return (
         <div id="ptnrelais">
-            <div id="widget-container"></div>
-                <input type="hidden" id="pudoWidgetErrorCode"/>
-                <input type="hidden" id="pudoWidgetAddress1"/>
-                <input type="hidden" id="pudoWidgetTown"/>
-                <input type="hidden" id="pudoWidgetZipCode"/>
+            <div id="widget-container">
+                
+            </div>
+            <input type="hidden" id="pudoWidgetErrorCode" onChange={(e)=>test(e)}/>
+            <input type="hidden" id="pudoWidgetAddress1" onChange={(e)=>test(e)}/>
+            <input type="hidden" id="pudoWidgetTown" onChange={(e)=>test(e)}/>
+            <input type="hidden" id="pudoWidgetCountry" onChange={(e)=>test(e)}/>
+            <input type="hidden" id="pudoWigdetType" onChange={(e)=>test(e)}/>
+            <input type="hidden" id="pudoWidgetZipCode" onChange={(e)=>test(e)}/>
         </div>
     )
 }
