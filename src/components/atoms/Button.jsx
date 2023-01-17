c02ca12702570e7096db332f84df3098c3ec471a
Emilien <emilien.ginko@gmail.com>
2023-01-17 19:43:13 +0100

    💄cursors

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Icon from "../atoms/Icon"
 import "../../assets/styles/components/atoms/buttons.scss"
-import { isDisabled } from "@testing-library/user-event/dist/utils";
 
 const Button = ({content, type, color, value, callBack, maxValue, isDisabled=false}, ...props)=>{
 
