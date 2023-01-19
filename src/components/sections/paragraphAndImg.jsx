78a21b56be2c1d95df43f55f89730272ca05c3ce
Emilien <emilien.ginko@gmail.com>
2023-01-19 22:18:12 +0100

    image and paragraph

---
+++ src/components/sections/paragraphAndImg.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+import desktopImg from "../../assets/images/pages/accueil/Desktop.jpg"
+import mobileImg from "../../assets/images/pages/accueil/Mobile.png"
+import "../../assets/styles/components/sections/stodacImgAndParagraph.scss"
+const PAndI = ()=>{
+    return(
+        <section className="stodac-img-and-paragraph">
+            <div className="paragraphs">
+                <h1>Notre expertise</h1>
+                <p>À travers Stodac, c’est plus de 10 ans d’expertise que nous mettons à votre disposition !</p>
+                <p>En effet, depuis notre création en 2013, nous avons dépannés la plupart des marques présentes sur le marché. </p>
+            </div>
+            <div className="imgs">
+                <picture>
+                    <source 
+                        srcSet={mobileImg}
+                        media="(max-width:768px)"
+                    />
+                    <source 
+                        srcSet={desktopImg}
+                    />
+                    <img src={desktopImg} alt="Illustration des stocks de stodac" />
+                </picture>
+            </div>
+        </section>
+    )
+    
+}
+export default PAndI
